096cf08ac6ef436c25524078e6d3ba0c80583338
Tkadha(home) <emforhs228@naver.com>
2024-05-19 17:17:24 +0900

    soccer 파싱

---
+++ soccer.py
@@ -0,0 +1,22 @@
+import requests
+import xml.etree.ElementTree as ET
+
+url='https://openapi.gg.go.kr/PublicTrainingFacilitySoccer'
+service_key="de852e4aa3fa49d09907453c21f5188c"
+Params={'Key': service_key,'Type': 'xml','pSize':265 }
+response = requests.get(url,params=Params)
+root = ET.fromstring(response.text)
+
+header = ["Name","sigun","Area","Material","x","y"]
+
+soccer_dicts=dict()
+for item in root.iter("row"):
+    name=item.findtext("FACLT_NM")
+    area=item.findtext("AR")
+    material=item.findtext("BOTM_MATRL_NM")
+    x=item.findtext("REFINE_WGS84_LAT")
+    y=item.findtext("REFINE_WGS84_LOGT")
+    sigun=item.findtext("SIGUN_NM")
+    soccer_dicts[name]=[sigun,area,material,x,y]
+
+
